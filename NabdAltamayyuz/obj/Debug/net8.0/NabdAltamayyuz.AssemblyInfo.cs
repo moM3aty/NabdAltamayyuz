4bbc6ca9423807c8a1c8d1e11c0fe54734c51071
moM3aty <mo.m3aty@gmail.com>
2026-02-20 00:09:41 +0200

    NabdAltamayyuz

--- NabdAltamayyuz/obj/Debug/net8.0/NabdAltamayyuz.AssemblyInfo.cs
+++ NabdAltamayyuz/obj/Debug/net8.0/NabdAltamayyuz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NabdAltamayyuz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cae063895175617914180881d8c3c15d7e1e85c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e286fb0c22f528f3a4b6a32700aa08fa2a25f378")]
 [assembly: System.Reflection.AssemblyProductAttribute("NabdAltamayyuz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NabdAltamayyuz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
